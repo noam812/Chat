d68ffa13286e0379200a3b0d13653b451eb44794
noam812 <noam812@gmail.com>
2022-04-03 20:49:08 +0300

    rdy 4 deploy

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === "production") {
 
   app.get("*", (req, res) => {
     res.sendFile(
-      path.resolve(__dirname, "client", "chat", "build", "index.html")
+      path.resolve(__dirname, "../client", "chat", "build", "index.html")
     );
   });
 }
